ff92b1f1570e810391637fab78f5ffe436dcb0bc
laana11 <m.pequena@icloud.com>
2024-11-13 23:12:12 +0300

    Добавлена кнопка для загрузки нового кота

--- Cats.py
+++ Cats.py
@@ -3,6 +3,30 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
+from Scripts.bottle import response
+
+
+def load_image(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        image_data = BytesIO(response.content)
+        img = Image.open(image_data)
+        return ImageTk.PhotoImage(img)
+    except Exception as e:
+        print(f"Произошла ошибка {e}")
+        return None
+
+
+def set_image():
+    img = load_image(url)
+
+    if img:
+        label.config(image=img)
+        label.image = img
+
+
+
 window = Tk()
 window.title("Cats")
 window.geometry("600x480")
@@ -10,11 +34,11 @@ window.geometry("600x480")
 label = Label()
 label.pack()
 
-url = "https://cataas.com/c at"
-img = load_image(url)
+update_button = Button(text="Обновить", command=set_image)
+update_button.pack()
 
-if img:
-    label.config(image=img)
-    label.image = img
+url = "https://cataas.com/cat"
+
+set_image()
 
 window.mainloop()
