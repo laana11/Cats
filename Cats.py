f3f7a019fc0feb0ebb1c1b843b450c3dc0b5de7c
laana11 <m.pequena@icloud.com>
2024-11-13 23:21:40 +0300

    Добавлено меню вместо кнопки

--- Cats.py
+++ Cats.py
@@ -12,7 +12,7 @@ def load_image(url):
         response.raise_for_status()
         image_data = BytesIO(response.content)
         img = Image.open(image_data)
-        img.thumbnail((600,480)), Image.Resampling.LANCZOS)
+        img.thumbnail((600,480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Произошла ошибка {e}")
@@ -27,6 +27,8 @@ def set_image():
         label.image = img
 
 
+def exit():
+    window.destroy()
 
 window = Tk()
 window.title("Cats")
@@ -35,8 +37,14 @@ window.geometry("600x520")
 label = Label()
 label.pack()
 
-update_button = Button(text="Обновить", command=set_image)
-update_button.pack()
+menu_bar = Menu(window)
+window.config(menu=menu_bar)
+
+file_menu = Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Загрузить фото", command=set_image)
+file_menu.add_separator()
+file_menu.add_command(label="Выход", command=exit)
 
 url = "https://cataas.com/cat"
 
